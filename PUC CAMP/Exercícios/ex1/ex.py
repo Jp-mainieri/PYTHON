51771b37b8bbfc8ebf4053990167060313c87439
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-02-14 17:10:31 -0300

    Initial commit

---
+++ PUC CAMP/Exercícios/ex1/ex.py
@@ -0,0 +1,4 @@
+number = 10
+numberAfter = number + 1
+numberBefore = number - 1
+print('your number is', number, 'the number before it is', numberBefore, 'and the number after it is', numberAfter )
---
+++ PUC CAMP/Exercícios/ex3/ex.py
@@ -0,0 +1,9 @@
+divisor = 3
+dividend = 11
+quotient = dividend // divisor
+remnant = dividend % divisor
+
+if (remnant != 0):
+    print('The result of the division of', dividend, 'by', divisor, 'is', quotient, 'and the remnant is', remnant)
+else:
+    print('The result of the division of', dividend, 'by', divisor, 'is', quotient)
