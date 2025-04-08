f47d88bf4488543b7ac60f732052e071a4a9edc1
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-04-07 21:32:32 -0300

    update T1ex8

--- PUC CAMP/25_1/T1 Algebra/ex8.py
+++ PUC CAMP/25_1/T1 Algebra/ex8.py
@@ -1,12 +1,13 @@
 A = [[0,0,0,0],[0,0,0,0],[0,0,0,0]]
-for i in range(3):
-    for j in range(4):
+for i in range(0,3):
+    for j in range(0,4):
         if i < j:
             A[i][j] = j-1
         if i > j:
             A[i][j] = 2*i
         if i == j:
             A[i][j] = i+j
-print(A)
-
+print(A[0])
+print(A[1])
+print(A[2])
         
