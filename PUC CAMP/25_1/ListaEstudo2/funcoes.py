5502fa204e3dc57abd58409bf6d6b3cdf310e53d
João Pedro Panza Mainieri <joaopedro.mainieri@gmail.com>
2025-05-29 21:36:13 -0300

    f

---
+++ PUC CAMP/25_1/ListaEstudo2/funcoes.py
@@ -0,0 +1,13 @@
+def printar_2(texto):
+    return(texto+texto)
+
+printar_2("Bet lindo")
+
+def somar_2(num1,num2):
+    somar = num1 + num2
+    return somar
+
+somar_4  = str(somar_2(10,20)) + printar_2(" Bet lindo")
+
+print(somar_4)
+
